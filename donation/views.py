473ab5a111e2b2730ad08224c721eb2fddc80895
Flesner96 <f.lesner@wp.pl>
2024-07-19 17:57:32 +0200

    add dynamic institutions

--- donation/views.py
+++ donation/views.py
@@ -7,9 +7,18 @@ class LandingPage(View):
     def get(self, request):
         total_bags = Bag.objects.aggregate(Sum('quantity'))['quantity__sum'] or 0
         total_institutions = Institution.objects.count()
+
+
+        fundations = Institution.objects.filter(type='fundacja')
+        organizations = Institution.objects.filter(type='organizacja pozarządowa')
+        collections = Institution.objects.filter(type='zbiórka lokalna')
+
         context = {
             'total_bags': total_bags,
             'total_institutions': total_institutions,
+            'fundations': fundations,
+            'organizations': organizations,
+            'collections': collections,
         }
         return render(request, 'index.html', context)
 class AddDonation(View):
